d4597d8132a2f97cccc0d3bb5894e35bbe1da477
jwallat <jwallat@hotmail.de>
2019-12-18 11:19:34 +0100

    removed faulty db onCreate statement that caused a no such tables error

--- lib/src/data/moor_database.dart
+++ lib/src/data/moor_database.dart
@@ -43,11 +43,5 @@ class AppDatabase extends _$AppDatabase {
         beforeOpen: (db) async {
           await customStatement('PRAGMA foreign_keys = ON');
         },
-        onCreate: (db) async {
-          // Add a default ingredient type as they are inherently needed but not currently used
-          IngredientTypeDao itDao = IngredientTypeDao(this);
-          itDao.insertIngredientType(
-              IngredientTypesCompanion(name: Value("<<placeholder>>")));
-        },
       );
 }
